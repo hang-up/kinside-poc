117732c9ac3f6870a2ceb901edb136ce7c761dc5
hang-up <robhang.up@gmail.com>
2022-08-24 19:00:17 -0400

    Created movie grid.

---
+++ frontend/src/components/MoviesGrid.vue
@@ -0,0 +1,38 @@
+<script>
+import MoviesGridItem from "./MoviesGridItem.vue";
+export default {
+  props: ["movies"],
+  components: {
+    // NOTE: A static import is needed since we are transitioning against it.
+    MoviesGridItem,
+  },
+};
+</script>
+
+<template>
+  <div class="movies-grid">
+    <TransitionGroup name="list" tag="moviesgriditem">
+      <MoviesGridItem
+        v-for="(movie, index) in movies"
+        :key="index"
+        :movie="movie"
+        class="movies-grid-item"
+      />
+    </TransitionGroup>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.movies-grid {
+  moviesgriditem {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  &-item {
+    height: 400px;
+    margin: 0.5rem;
+    flex: 0 0 calc(25% - 1rem);
+  }
+}
+</style>
