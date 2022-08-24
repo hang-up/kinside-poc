70a9c255c64694e2269231425649716297b835c8
hang-up <robhang.up@gmail.com>
2022-08-24 18:59:19 -0400

    Created movies store.

---
+++ frontend/src/stores/movies.js
@@ -0,0 +1,30 @@
+import { defineStore } from "pinia";
+import { shallowRef } from "vue";
+
+const useMoviesStore = defineStore({
+  id: "movies",
+  state: () => ({
+    movies: shallowRef([]),
+    filterBy: "",
+  }),
+  getters: {
+    /**
+     * Returns a list of filtered movies based on the search query.
+     *
+     * @param {Object} state Current state
+     * @returns Array Array of filtered movies based on the search query
+     */
+    filteredMovies: (state) => {
+      if (!state.filterBy.length) {
+        return state.movies;
+      }
+      return state.movies.filter((movie) =>
+        movie.title
+          .toLocaleLowerCase()
+          .includes(state.filterBy.toLocaleLowerCase())
+      );
+    },
+  },
+});
+
+export { useMoviesStore };
